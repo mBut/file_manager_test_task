ae3308ec66a4b26b95cb69b68eebca59be26aced
Mihails Butorins <mihails.butorins@gmail.com>
2015-10-27 11:32:17 -0400

    Added ideas.txt file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'file_manager#index'
 
   #post 'file_manager/file_upload_credentials' => 'file_manager#file_upload_credentials'
 
